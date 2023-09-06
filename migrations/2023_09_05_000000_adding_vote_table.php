7df53856d772d1d3a04ff3caa64dcabb6980f8c9
Mike Lundahl <4yv20gke1pno48zosc8ni9zwm5l7to@bots.bitbucket.org>
2023-09-06 12:57:21 +0200

    tweaks

--- migrations/2023_09_05_000000_adding_vote_table.php
+++ migrations/2023_09_05_000000_adding_vote_table.php
@@ -13,18 +13,18 @@ use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
 return Migration::createTable('mbl_featured_projects_votes', function (Blueprint $table) {
+
     $table->increments('id');
 
     $table->integer('user_id')->unsigned()->nullable();
-    $table->string('user_name');
+    $table->string('user_name', 100);
 
     $table->integer('discussion_id')->unsigned();
-    $table->string('discussion_slug')->unsigned();
 
     $table->timestamp('vote_date')->nullable();
     $table->timestamps();
 
     $table->foreign('discussion_id')->references('id')->on('discussions')->onDelete('cascade');
-    $table->foreign('discussion_slug')->references('slug')->on('discussions')->onUpdate('cascade');
     $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+    $table->foreign('user_name')->references('username')->on('users')->onUpdate('cascade');
 });
